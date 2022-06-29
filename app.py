bd73081965cdec3d6f562c610da787060e847551
Ephraim Becker <emb16@outlook.com>
2022-06-28 20:43:01 -0400

    Started work on the countdown.

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/countdown/")
+def countdown():
+    return render_template('countdown/index.html')
